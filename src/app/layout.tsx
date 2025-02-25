f3d66b278eb9a3393d2c0592de3f2301fd6e4853
joelC16 <joelcordero728@gmail.com>
2025-02-25 17:52:52 -0300

    agregue google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,13 +21,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R76YXKB1XT"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-R76YXKB1XT');
+            `,
+          }}
+        />
+      </head>
 
       <body className={`${unbounded.variable} ${poppins.variable} antialiased`}>
         <SmoothScroll>
           {children}
         </SmoothScroll>
-        <SpeedInsights/>
+        <SpeedInsights />
       </body>
     </html>
   );
