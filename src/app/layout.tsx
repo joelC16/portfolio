2b59a971c9e36160bf03b3471122b37f3d422b94
joelC16 <joelcordero728@gmail.com>
2025-02-25 17:56:44 -0300

    agregue las analiticas gratis de vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Unbounded, Poppins } from "next/font/google";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react"
+
 import SmoothScroll from "@/components/SmoothScroll";
 
 const unbounded = Unbounded({
@@ -42,6 +44,7 @@ export default function RootLayout({
           {children}
         </SmoothScroll>
         <SpeedInsights />
+        <Analytics/>
       </body>
     </html>
   );
